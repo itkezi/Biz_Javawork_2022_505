c0a6076b169abf174920dcc86563cbd37752d61f
itkezi <dkdlel816@naver.com>
2022-04-12 08:18:44 +0900

    220411

---
+++ Java_30_Arrays_02/src/com/callor/arrays/ArraysEx.java
@@ -0,0 +1,20 @@
+package com.callor.arrays;
+
+public class ArraysEx {
+
+	public static void main(String[] args) {
+
+		int index = 0;
+		for (index = 0; index < 10; index++) {
+			System.out.println(index);
+
+		}
+		
+		for (int i= 0; i<10 ; i++) {
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
