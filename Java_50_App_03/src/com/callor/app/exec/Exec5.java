3d31ccb7c1c4e64e625c0bea17a23d1c43d8d202
itkezi <dkdlel816@naver.com>
2022-04-27 12:38:59 +0900

    220427

--- Java_50_App_03/src/com/callor/app/exec/Exec5.java
+++ Java_50_App_03/src/com/callor/app/exec/Exec5.java
@@ -1,31 +1,28 @@
 package com.callor.app.exec;
 
-public class Exec4 {
+public class Exec5 {
 
 	public static void main(String[] args) {
 
 		// num에 담겨있는 값이 소수인가
 		int num = (int) (Math.random() * 100) + 1;
-		int flag = 0;
-		
-		if(num == 1) {
-			System.out.printf("나온 값이 %d입니다.", num );;
+
+		if (num == 1) {
+			System.out.printf("나온 값이 %d입니다.\n", num);
 		}
-		
-		int i =0 ;
-		for ( i = 2; i < num; i++) {
+
+		int i = 0;
+
+		for (i = 2; i < num; i++) {
 			if (num % i == 0) {
-				flag = 1;
 				break;
 			} // end if
 		} // end for
 		
-		if (flag == 0)
-			System.out.print(num + "은(는) 소수입니다.");
-		else
+		if (i < num)
 			System.out.print(num + "은(는) 소수가 아닙니다.");
-		
+		else
+			System.out.print(num + "은(는) 소수입니다.");
+
 	} // end main
 } // end class
-
-
