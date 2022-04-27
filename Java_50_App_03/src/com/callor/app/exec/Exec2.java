73fe1e809b886f3d1b241e2e7b638e7f6665c714
itkezi <dkdlel816@naver.com>
2022-04-27 10:33:32 +0900

    220427

---
+++ Java_50_App_03/src/com/callor/app/exec/Exec2.java
@@ -0,0 +1,30 @@
+package com.callor.app.exec;
+
+public class Exec2 {
+	
+	// 정수형 배열 10개를 생성하고 각 요소에 1 ~ 100 까지 임의 정수를 생성하여 저장하고
+	// 배열에 저장된 요소의 값을 모두 합산하여 출력
+	public static void main(String[] args) {
+		
+		int[] intNums = new int[10];
+		int intSum = 0;
+		
+		for(int i = 0 ; i < intNums.length ; i ++) {
+			
+			int num = (int)(Math.random()* 100 ) + 1 ;
+			intNums[i] = num;
+			
+		}
+		
+		for(int i : intNums) {
+			intSum += intNums[i];
+			
+		}
+			
+		System.out.println(intSum);
+		
+				
+		
+	}
+
+}
---
+++ Java_50_App_03/src/com/callor/app/exec/Exec4.java
@@ -0,0 +1,24 @@
+package com.callor.app.exec;
+
+public class Exec4 {
+
+	public static void main(String[] args) {
+
+		// num에 담겨있는 값이 소수인가
+		int num = (int) (Math.random() * 100) + 1;
+		int flag = 0;
+		
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 0)
+			System.out.print(num + "은(는) 소수입니다.");
+		else
+			System.out.print(num + "은(는) 소수가 아닙니다.");
+
+	}
+
+}
